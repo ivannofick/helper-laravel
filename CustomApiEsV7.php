93b2ab623e3fadcffa9eb7dde488ba235f4ec2f8
ivannofick\windows <ivannofick>
2021-12-15 22:54:41 +0700

    add commit

--- CustomApiEsV7.php
+++ CustomApiEsV7.php
@@ -2,40 +2,80 @@
 
 namespace App\Helpers;
 use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+
 
 /**
  * 
  * @author ivannofick
  * 
- * helper custom Query using Elastic search
+ * helper custom Query using Elastic search version 7.15
  * 
  * you can use it, just call class it on a controller
  * ruangapp.com
  */
 class CustomApiEsV7
 {
-  public function createIndex($idIndex, $data, $index)
+
+  /**
+   * create index
+   * 
+   * @param int $idIndex
+   * @param array $data
+   * @param string $index
+   * 
+   * @return array $data
+   */
+  public static function createIndex($idIndex=null, $data=null, $index=null)
   {
-    $request = $httpClient->request('PUT', config('services.elasticsearch.base_url').$index."/"."_doc/".$idIndex, [
-        'headers' => [
-            'accept' => 'application/json'
-        ],
-        'json' => $data
-    ]);
-    $dataResponse = json_decode($request->getBody()->getContents());
-    return (array)$dataResponse;
+    $resCreate = Http::post(config('services.elasticsearch.base_url')."/".$index."/"."_doc/".$idIndex, (array)$data)->body();
+    $data = json_decode($resCreate, true);
+    return $data;
   }
 
-  public function getIndex($idIndex, $data, $index)
+  /**
+   * get data in index ES
+   * 
+   * @author ruangapp
+   * @param Int $idIndex
+   * @param Array $data
+   * @param String $index
+   * @return array $dataResponse
+   */
+  public static function getIndex($idIndex=null, $data=null, $index=null, $search=null)
   {
-   $request = $httpClient->request('GET', config('services.elasticsearch.base_url').$index."/"."_search", [
-        'headers' => [
-            'accept' => 'application/json'
-        ],
-        'json' => $data
-    ]);
-    $dataResponse = json_decode($request->getBody()->getContents());
-    return (array)$dataResponse;
+    $arr = [
+      "query" => [
+            "match" => [
+                "title" => $search  
+            ]
+      ]
+    ];
+    $response = Http::withHeaders(['Content-Type' => 'application/json'])
+    ->send('GET', config('services.elasticsearch.base_url')."/".$index."/"."_search?filter_path=hits.hits._source", [
+        'json' => $arr
+    ])->body();
+    $json = json_decode($response, true);
+
+    if (count($json) < 1 || !isset($json["hits"])) {
+      $json = [
+        "hits" => [
+          "hits" => [
+            [
+              "_source" => ["message" => "data tidak ditemukan"]
+            ]
+          ]
+        ]
+      ];
+    }
+    $dataEs = array_map(function ($i) {
+        if (!isset($i["_source"]["message"])) {
+            return $i["_source"];
+        }
+    }, $json["hits"]["hits"]);
+    if ($dataEs[0] != null) {
+        return $dataEs;
+    }
   }
 
 
